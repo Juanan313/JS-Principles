ddd06092ec2ffba26597ae3caf29e49550f1a96e
juanan <juanan.pujals@gmail.com>
2018-10-08 17:02:43 +0200

    3rdVariableScope

--- variable_scope_03.js
+++ variable_scope_03.js
@@ -21,3 +21,9 @@ function checkscope() {
 
 console.log(checkscope());              // => "nested scope"
 console.log(scope);                     // => "global scope"
+
+/*
+    Al hacer un log del resultado de la función la función nested devuelve la variable instanciada 
+    dentro de si misma, por lo que no utiliza el valor de la funcion checkscope() y el segundo
+    log muestra el valor del scope global
+*/
