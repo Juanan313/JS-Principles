e9ad41930e4155b9d896544643d6f3cf867fdc50
juanan <juanan.pujals@gmail.com>
2018-10-08 17:10:36 +0200

    5thVariableScope

--- variable_scope_05.js
+++ variable_scope_05.js
@@ -16,3 +16,11 @@ function f() {
 f();
 
 console.log(scope);                 // Prints "global"
+
+/*
+    Cómo no se le esta pasando en ningun momento la variable gloabl scope a la función
+    y se esta declarando esta misma en la función no la busca fuera de esta y cuando ejecuta la
+    línea del primer log antes de inicializarla, mientras como no ha ido a buscar fuera de la
+    función no se ha ejecutado la inicialización anterior, y el segundo log devuelve el valor de
+    local por que ya se ha instanciado. 
+*/
