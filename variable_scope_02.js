df6cc394dc2c0c1d741235cb5f46275c3d1beee3
juanan <juanan.pujals@gmail.com>
2018-10-08 16:57:03 +0200

    2ndVariableScope

--- variable_scope_02.js
+++ variable_scope_02.js
@@ -18,3 +18,11 @@ console.log(checkscope2());
 
 console.log(scope);
 console.log(myscope);           // => WTF ¿?
+
+/*
+ El primer cl devuelve un array con los valores asignados dentro de la función
+mientras que los otros dos devuelven estos mismos valores pero de cada una de la variables
+la primera de estas dos variables sobrescribe a la variable scope="global";
+y myscope al no declararse como var o let no se le esta dando un ambito de uso a 
+la variable
+*/ 
